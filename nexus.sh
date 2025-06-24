d51cc0adc9f433de7674a926587dc553186fef82
kangjk <96231886+KangJKJK@users.noreply.github.com>
2025-06-24 21:36:51 +0900

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -24,7 +24,7 @@ sudo make install
 rm -rf $HOME/.nexus
 mkdir -p $HOME/.nexus
 cd $HOME/.nexus
-wget https://raw.githubusercontent.com/kooroot/Node_Executor-Nexus/refs/heads/main/nexus_s3.sh
+wget -O https://raw.githubusercontent.com/kooroot/Node_Executor-Nexus/refs/heads/main/nexus_s3.sh
 chmod +x nexus_s3.sh
 
 # 3. nexus_s3.sh 수정
